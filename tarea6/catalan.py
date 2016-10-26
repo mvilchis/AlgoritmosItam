c16279f05c6f1b27c7c9e309e48c6da975dd50a1
Your Name <mvilchis@ciencias.unam.mx>
2016-10-26 00:28:23 -0500

    Catalan funcionando

--- tarea6/catalan.py
+++ tarea6/catalan.py
@@ -1,43 +1,57 @@
 import profile
 import sys
 import getopt
+import time 
 
+
+counter_cat = 0 
+counter_cat_mem = 0
 def num_catalan(n):
-    if n == 1:
+    global counter_cat
+    counter_cat += 1
+    if n == 1: 
         return 1
+    if n == 2:
+        return 2
     else:
         counter = 0
         for i in range(1,n-1):
-            counter += num_catalan(i)* num_catalan(n-1)
+            counter += num_catalan(i)* num_catalan(n-i) 
         return counter
 
 
-
 def num_catalan_mem(n, Matrix):
+    global counter_cat_mem
+    counter_cat_mem += 1
     if n == 1:
         return 1
+    if n == 2:
+        return 2
     else:
         counter = 0
         for i in range(1,n-1):
             cat_i = Matrix[i] if Matrix[i] != -1 else num_catalan_mem(i, Matrix)
-            cat_n_1 = Matrix[n-1] if Matrix[n-1] != -1 else num_catalan_mem(n-1, Matrix)
-            counter += cat_i *cat_n_1
-
+            cat_n_1 = Matrix[n-i] if Matrix[n-i] != -1 else num_catalan_mem(n-i, Matrix)
+            counter += cat_i *cat_n_1 
         return counter
 
 def main(recursivo = False):
-    n_max = 100
+    n_max = 200 
     if recursivo:
+        global counter_cat 
         for n in range(1, n_max):
-            print "*"*10
-            print "%d" %(n)
-            profile.run('num_catalan(%d)' %(n))
+            counter_cat = 0
+            start = time.time()
+            num_catalan(n)
+            print "%d %s %d"% (n, time.time() - start, counter_cat)
     else:
+        global counter_cat_mem
         for n in range(1, n_max):
             Matrix = [-1 for x in range(n+1)]
-            print "*"*10
-            print "%d" %(n)
-            profile.runctx('print num_catalan_mem(n,Matrix); print', globals(), {'n':n, 'Matrix':Matrix})
+            counter_cat_mem = 0
+            start = time.time()
+            num_catalan_mem(n, Matrix)
+            print "%d %s %d"% (n, time.time() - start, counter_cat_mem)
 
 if __name__ == '__main__':
     argv = sys.argv[1:]
